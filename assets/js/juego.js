949240e44d997883532c6ed825390c62dd3d5feb
Daniel Gómez H <danielivgho5@gmail.com>
2023-08-06 21:27:41 -0500

    Función para conocer el valor de las cartas agregada

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -48,3 +48,13 @@ const pedirCarta = () => {
 };
 
 pedirCarta();
+
+//Función para conocer el valor de las cartas
+
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
+
+const valor = valorCarta(pedirCarta());
+console.log(valor);
